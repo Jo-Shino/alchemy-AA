70327cdb70697b1fdfc9e0c824771cda2f66de44
Jo-Shino <joe000042@gmail.com>
2024-11-01 08:33:11 +0900

    add/➕ contractオブジェクトをroute.tsで取得

--- my-app/src/app/api/example_contract/route.ts
+++ my-app/src/app/api/example_contract/route.ts
@@ -1,2 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import ExampleContract from "../../../../../contract/artifacts/contracts/Example.sol/Example.json";
+
+const exampleContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhatのローカル
+
 export const POST = async (req: NextRequest) => {};
