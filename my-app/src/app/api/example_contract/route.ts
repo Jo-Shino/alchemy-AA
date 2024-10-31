7cde3edf652af9a7e9ed4279a1f0e441838ce662
Jo-Shino <joe000042@gmail.com>
2024-11-01 08:44:46 +0900

    add/➕　必要な要素を追加

--- my-app/src/app/api/example_contract/route.ts
+++ my-app/src/app/api/example_contract/route.ts
@@ -2,5 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import ExampleContract from "../../../../../contract/artifacts/contracts/Example.sol/Example.json";
 
 const exampleContractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // hardhatのローカル
+const exampleContractAbi = ExampleContract.abi;
+const privateKey = process.env.PRIVATE_KEY;
 
 export const POST = async (req: NextRequest) => {};
